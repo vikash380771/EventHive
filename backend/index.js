c2168448ce934cfa598e983d898e214d1ec66072
Vikash Bharti <125040065+vikash380771@users.noreply.github.com>
2026-02-18 19:45:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
@@ -58,6 +57,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
